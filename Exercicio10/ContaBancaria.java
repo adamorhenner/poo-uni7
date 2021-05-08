c4563ebd0e3dcfe5e34871a3e0f604e7892f3c77
Adamor Henner <adacandido9@gmail.com>
2021-05-08 20:36:57 -0300

    Limpando codigo

--- Exercicio10/ContaBancaria.java
+++ Exercicio10/ContaBancaria.java
@@ -1,10 +1,6 @@
 
 
 public abstract class ContaBancaria {
-//	  private String name;
-//	  private int cpf;
-//	  private int conta;
-//	  private int agencia;
 	  protected double saldo;
 	  private Pessoa Cliente;
 	  
@@ -24,48 +20,6 @@ public abstract class ContaBancaria {
 	}
 
 	 
-	  
-//	  public String getName() {
-//		return name;
-//	}
-//
-//	public void setName(String nome) {
-//		this.name = nome;
-//	}
-//
-//	public String getCpf() {
-//		return cpf;
-//	}
-//
-//	public void setCpf(int cpf2) {
-//		this.cpf = cpf2;
-//	}
-//
-//	public int getConta() {
-//		return conta;
-//	}
-//
-//	public void setConta(int conta) {
-//		this.conta = conta;
-//	}
-//
-//	public int getAgencia() {
-//		return agencia;
-//	}
-//
-//	public void setAgencia(int agencia) {
-//		this.agencia = agencia;
-//	}
-//
-//	public double getSaldo() {
-//		return saldo;
-//	}
-//
-//	public void setSaldo(double saldo) {
-//		this.saldo = saldo;
-//	}
-
-
 	
 
 public void tranferencia(ContaBancaria conta,double valor) throws DepositoNegativoException, TransferenceException {
